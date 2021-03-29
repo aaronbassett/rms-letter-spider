3a317f24ddc54e1e965c5a1cf1f3c4834a7d349d
Alexander Zaitsev <zamazan4ik@tut.by>
2021-03-30 01:05:56 +0300

    feat: add parser
    - add parser for parsing github usernames for all RMS enemies
    
    Tested:
    - Several local tests

--- spider.py
+++ spider.py
@@ -2,7 +2,18 @@ import httpx
 from bs4 import BeautifulSoup
 
 github_usernames = []
-response = httpx.get('https://rms-support-letter.github.io/')
+response = httpx.get('https://rms-open-letter.github.io/')
+
+
+def find_github_username_end_index(string, start_index):
+    i = start_index
+    while i < len(string):
+        if string[i] == ',' or string[i] == ')' or string[i] == ' ' or string[i] == '/' or string[i] == '\n':
+            return i
+        i = i + 1
+
+    return None
+
 
 if response.status_code == 200:
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -11,5 +22,11 @@ if response.status_code == 200:
 
         if url.find("https://github.com") != -1:
             github_usernames.append(url.strip("/").split("/")[-1])
+
+    for text in soup.select('ol li:-soup-contains("@")'):
+        raw_text = text.getText()
+        github_username_start = raw_text.find('@')
+        github_username_end = find_github_username_end_index(raw_text, github_username_start)
+        github_usernames.append(raw_text[github_username_start + 1:github_username_end])
 
 print(github_usernames)
