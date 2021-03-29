5026c9ca6eca44c3ddc7f0c028d1f3263bf16d33
Aaron Bassett <me@aaronbassett.com>
2021-03-29 14:22:32 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# RMS Open Letter Spider
+
+This spider collects a list of the GitHub usernames of the RMS open letter of support signatories for use with the [rms-letter-sigs Chrome Extension](https://github.com/aaronbassett/rms-letter-sigs)
+
+## Usage
+
+Clone the repository and install the dependencies with `pip install -r requirements.txt` and run with `python server.py`
---
+++ spider.py
@@ -0,0 +1,15 @@
+import httpx
+from bs4 import BeautifulSoup
+
+github_usernames = []
+response = httpx.get('https://rms-support-letter.github.io/')
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, 'html.parser')
+    for link in soup.select('ol li a'):
+        url = link.get('href')
+
+        if url.find("https://github.com") != -1:
+            github_usernames.append(url.strip("/").split("/")[-1])
+
+print(github_usernames)
